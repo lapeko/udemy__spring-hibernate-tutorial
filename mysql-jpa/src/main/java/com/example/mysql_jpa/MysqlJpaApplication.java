9da862c94d0a64d6d1adb7fbea8414762215f85d
Vitali Lapeka <lapeko88@gmail.com>
2025-06-27 22:20:38 +0200

    add log on run

--- mysql-jpa/src/main/java/com/example/mysql_jpa/MysqlJpaApplication.java
+++ mysql-jpa/src/main/java/com/example/mysql_jpa/MysqlJpaApplication.java
@@ -1,7 +1,9 @@
 package com.example.mysql_jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MysqlJpaApplication {
@@ -10,4 +12,10 @@ public class MysqlJpaApplication {
 		SpringApplication.run(MysqlJpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("App works");
+		};
+	}
 }
