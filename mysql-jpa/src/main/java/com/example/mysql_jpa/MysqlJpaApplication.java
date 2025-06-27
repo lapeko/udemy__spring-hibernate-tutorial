9e2059e7354c6ab533c762e28512a830464866f9
Vitali Lapeka <lapeko88@gmail.com>
2025-06-27 23:37:01 +0200

    create a user on app run

--- mysql-jpa/src/main/java/com/example/mysql_jpa/MysqlJpaApplication.java
+++ mysql-jpa/src/main/java/com/example/mysql_jpa/MysqlJpaApplication.java
@@ -1,5 +1,7 @@
 package com.example.mysql_jpa;
 
+import com.example.mysql_jpa.dao.StudentDAO;
+import com.example.mysql_jpa.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,18 @@ public class MysqlJpaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner() {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return args -> {
-			System.out.println("App works");
+			saveStudent(studentDAO);
 		};
+	}
+
+	private void saveStudent(StudentDAO studentDAO) {
+		var tempStudent = new Student("Alexandra", "Granma", "granma@mail.com");
+
+		System.out.printf("Saving a user: %s%n", tempStudent);
+		studentDAO.save(tempStudent);
+
+		System.out.printf("User: %s successfully saved%n", tempStudent);
 	}
 }
