acffdd4efb2e9402ac487b90ee90a9580ad2a279
Vitali Lapeka <lapeko88@gmail.com>
2025-07-02 00:30:58 +0200

    add route with path var

--- 3-rest/src/main/java/com/example/rest_crud/controller/MainController.java
+++ 3-rest/src/main/java/com/example/rest_crud/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.example.rest_crud.controller;
 
 import com.example.rest_crud.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,23 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/api")
 public class MainController {
+    private ArrayList<Student> students;
+
+    @PostConstruct
+    private void load() {
+        students = new ArrayList<Student>();
+        students.add(new Student("Mickey", "Mouse"));
+        students.add(new Student("Thomas", "Jasper Cat Sr"));
+        students.add(new Student("G.G. Goofy", "Goof"));
+    }
+
     @GetMapping("/students")
     public ArrayList<Student> getStudents() {
-        var students = new ArrayList<Student>();
-        students.add(new Student("Mickey", "Mouse"));
-        students.add(new Student("Tom", "Tom"));
         return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable("id") int id) {
+        return students.get(id);
     }
 }
