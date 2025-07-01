a1a94e09dd93c75e41ec9c32ed87de590459d563
Vitali Lapeka <lapeko88@gmail.com>
2025-07-02 00:10:33 +0200

    add student controller and entity

--- 3-rest/src/main/java/com/example/rest_crud/controller/MainController.java
+++ 3-rest/src/main/java/com/example/rest_crud/controller/MainController.java
@@ -1,14 +1,20 @@
 package com.example.rest_crud.controller;
 
+import com.example.rest_crud.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api")
 public class MainController {
-    @GetMapping("/great")
-    public String greet() {
-        return "Hi there";
+    @GetMapping("/students")
+    public ArrayList<Student> getStudents() {
+        var students = new ArrayList<Student>();
+        students.add(new Student("Mickey", "Mouse"));
+        students.add(new Student("Tom", "Tom"));
+        return students;
     }
 }
